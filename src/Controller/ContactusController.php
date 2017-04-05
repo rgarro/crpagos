a43483f98e0acd740c02ee5302acd810c350adea
rgarro <rolando@emptyart.xyz>
2017-04-04 19:59:08 -0600

    testing mailer III

--- src/Controller/ContactusController.php
+++ src/Controller/ContactusController.php
@@ -14,7 +14,7 @@ class ContactusController extends AppController
     $session = $this->request->session();
     if(count($_POST) > 0){
       //$FormID = $this->Forms->index();
-      $SafeMail = $_POST['Email'];//Sanitize::paranoid($_POST['Email'], array('@', '.'));contact_spa_cr.ctp
+      $SafeMail = $_POST['Email'];
       switch ($_POST['FormType']){
         case 1:
           $EmailSubject = __('BusinessForm', true);
@@ -28,20 +28,8 @@ class ContactusController extends AppController
       }
 
       $EmailSubject .=" # ".date("F j, Y, g:i a");
-      $this->set('Title', $EmailSubject);
-      /*$this->SwiftMailer->charset = "iso-8859-1";
-      $this->SwiftMailer->from = "info@crpagos.com";
-      $this->SwiftMailer->fromName = "Servicio Al Cliente";
-      $this->SwiftMailer->to = array('info@crpagos.com'=>'Servicio Al Cliente');
-      $this->SwiftMailer->bcc = array("bcc@grupochanto.com"=>"Administrator");
-      $this->SwiftMailer->replyTo = "info@crpagos.com";
-      $this->SwiftMailer->sendAs = "html";
-      if (!$this->SwiftMailer->send('contact_'.$session->read('LocaleCode'), $EmailSubject)) {
-          $this->log('Error sending email "register".', LOG_ERROR);
-      }*/
-      //$Email = new CakeEmail();
       $Email = new Email('default');
-      //$Email->viewVars(array('SearchQ'=>$this->viewVars['SearchQ']));
+      $Email->viewVars(array('Title'=>$EmailSubject));
       $Email->emailFormat('html');
       $Email->template('contact_'.$session->read('LocaleCode')."_html");
       $Email->from(array('info@crpagos.com' => 'InfoCRPagos'));
