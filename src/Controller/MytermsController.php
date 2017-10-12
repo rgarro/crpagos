fd232d79abbc8b19288e47d7d7c9204dc23e7ec9
rgarro <rgarro@gmail.com>
2017-10-11 20:23:13 -0600

    [Delivers #151767377] fixing terms update

--- src/Controller/MytermsController.php
+++ src/Controller/MytermsController.php
@@ -3,13 +3,15 @@ namespace App\Controller;
 
 use App\Controller\AppController;
 use Cake\I18n\I18n;
+use Cake\Core\Configure;
+use Cake\Mailer\Email;
+use Cake\Core\App;
 use App\Lib\L10n;
-use Cake\Controller\Component\CookieComponent;
 
 class MytermsController extends AppController{
 
   public function terms(){
     $this->viewBuilder()->setLayout('ajax');
   }
-  
+
 }
--- src/Template/Myterms/terms.ctp
+++ src/Template/Myterms/terms.ctp
@@ -1,2 +1,7 @@
 <?php
-print_r($_SESSION);
+use Cake\Core\App;
+if($_SESSION['LocaleCodeb'] == "es_CR"){
+  require current(App::path("Template")).'/Myterms'.DS.'es.ctp';
+}else{
+  require current(App::path("Template")).'/Myterms'.DS.'en.ctp';
+}
